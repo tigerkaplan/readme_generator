9d9efcbb2bcfff55f1e5a0a23e7b7642605689d1
tigerkaplan <husniyeerparun@gmail.com>
2023-12-17 19:08:05 +0000

    commits added

--- index.js
+++ index.js
@@ -5,47 +5,47 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-  {
+  {//username
     type: "input",
     name: "username",
     message: "GitHub username:"
   },
-  {
+  {//email
     type: "input",
     name: "email",
     message: "GitHub email address:"
   },
-  {
+  {//deployment
     type: "input",
     name: "deployment",
     message: "Deployment:"
   },
-  {
+  {//demo
     type: "input",
     name: "link",
     message: "Demo:"
   },
-  {
+  {//repo
     type: "input",
     name: "repository",
     message: "GitHub repository:"
   },
-  {
+  {//title
     type: "input",
     name: "title",
     message: "Project title:"
   },
-  {
+  {//description
     type: "input",
     name: "description",
-    message: "Brief project description :"
+    message: "Brief project description:"
   },
-  {
+  {//screenshots
     type: "input",
     name: "screenshot",
-    message: "Project images / screenshot"
+    message: "Project images / screenshot:"
   },
-  {
+  {//installation
     type: "input",
     name: "installation",
     message: "Required installation/s:"
@@ -53,10 +53,9 @@ const questions = [
   {//Project language
     type: "input",
     name: "require",
-    message: "Project dependencies/language:",
+    message: "Project language:",
   },
-  {
-    type: "input",
+  {//project purpose    type: "input",
     name: "usage",
     message: "Project usage/purpose:"
   },
@@ -66,17 +65,17 @@ const questions = [
     message: "Project license:",
     choices: ['None','GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
   },
-  {
+  {//contributing
     type: "input",
     name: "contribute",
     message: "Contributor's GitHub username/s:"
   },
-  {
+  {//test
     type: "input",
     name: "tests",
     message: "Project tests:"
   },
-  {
+  {//question
     type: "input",
     name: "questions",
     message: "Questions:"
