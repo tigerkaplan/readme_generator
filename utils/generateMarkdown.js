ba7878dccf257bf160c600737135660d86fbac68
tigerkaplan <husniyeerparun@gmail.com>
2023-12-17 03:11:05 +0000

    working on function generateMarkdownw

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,29 @@ function licenseBadge(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 ${licenseBadge(data.license)}
-
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Languages & Dependencies
+${data.require}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contribute}
+## Testing
+${data.tests}
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.username}](https://github.com/${data.username}) for my other projects on GitHub.
 `;
 }
 
