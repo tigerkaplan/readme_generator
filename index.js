5d008fdcf97b7a85c85edd19e77a38ba44e2157b
tigerkaplan <husniyeerparun@gmail.com>
2023-12-17 00:06:26 +0000

    Writing ReadMe file

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
